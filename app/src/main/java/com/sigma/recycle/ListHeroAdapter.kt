47dc8f232ad74840adcbe86b0b56ab229f9960b5
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-03-23 15:06:05 +0700

    belajarRecycleview

--- app/src/main/java/com/sigma/recycle/ListHeroAdapter.kt
+++ app/src/main/java/com/sigma/recycle/ListHeroAdapter.kt
@@ -7,28 +7,38 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListHeroAdapter(private val listHero : ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, i: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
         holder.imgPhoto.setImageResource(photo)
-        holder.tvName.text =name
+        holder.tvName.text = name
         holder.tvDescription.text = description
-    }
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
+        }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
-        return ListViewHolder(view)
     }
 
     override fun getItemCount(): Int = listHero.size
 
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val imgPhoto : ImageView = itemView.findViewById(R.id.img_item_photo)
-    val tvName : TextView = itemView.findViewById(R.id.tv_item_name)
-    val tvDescription : TextView = itemView.findViewById(R.id.tv_item_description)
-
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
     }
 
-
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
 }
